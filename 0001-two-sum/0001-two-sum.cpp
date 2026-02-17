197d7e8d6fcf32e771515754e8f88e20bd9a6064
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-02-17 18:50:49 +0600

    Time: 1 ms (77.27%), Space: 14.8 MB (36%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> index;
+        vector<int> result;
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if(index.count(complement)) {
+                result.push_back(index[complement]);
+                result.push_back(i);
+                break;
+            }
+            else index[nums[i]] = i;
+        }
+        return result;
+    }
+};
