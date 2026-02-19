00f150ff1db2ded2d9758a9fa06d34c5358eecbf
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-02-20 00:04:13 +0600

    Time: 4 ms (16.31%), Space: 10.1 MB (30.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char> alnum_arr;
-        for(char ch : s) {
-            if(isalnum(ch)) alnum_arr.push_back(tolower(ch));
-        }
-        int n = alnum_arr.size();
-        if(n == 1) return true;
-        for(int i = 0, j = n - 1; i < j; i++, j--) {
-            if(alnum_arr[i] != alnum_arr[j]) return false;
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            while(i < j && !isalnum(s[i])) i++;
+            while(j > i && !isalnum(s[j])) j--;
+            if(tolower(s[i]) != tolower(s[j])) return false;
         }
         return true;
     }
