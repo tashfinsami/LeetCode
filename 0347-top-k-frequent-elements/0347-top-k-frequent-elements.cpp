217eafb924ce16cb4c08451fd9e49c38e7c5d7ad
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-02-19 17:18:04 +0600

    Time: 7 ms (16.73%), Space: 18.1 MB (40.25%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,11 +3,15 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> freq;
         for(const auto& val : nums) freq[val]++;
-        vector<pair<int, int>> freq_arr;
-        for(const auto& pair : freq) freq_arr.push_back({pair.second, pair.first});
-        sort(freq_arr.begin(), freq_arr.end(), greater<pair<int, int>>());
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+        for(const auto& pair : freq) min_heap.push({pair.second, pair.first});
+        int extra = min_heap.size() - k;
+        while(extra--) min_heap.pop();
         vector<int> result;
-        for(int i = 0; i < k; i++) result.push_back(freq_arr[i].second);
+        while(!min_heap.empty()) {
+            result.push_back(min_heap.top().second);
+            min_heap.pop();
+        }
         return result;
     }
 };
