fec998edf231e856c858cfc5398fdf5380dbab59
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-05 00:18:59 +0600

    Time: 27 ms (11.48%), Space: 14.4 MB (20.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,12 +5,12 @@ public:
         int l = 0, r = 0;
         unordered_set<char> char_set;
         while(r < s.size()) {
-            if(char_set.find(s[r]) != char_set.end()) {
+            //if(char_set.find(s[r]) != char_set.end()) {
                 while(char_set.find(s[r]) != char_set.end()) {
                     char_set.erase(s[l]);
                     l++;
                 }
-            }
+            //}
             char_set.insert(s[r]);
             count = max(count, r - l + 1);
             r++;
