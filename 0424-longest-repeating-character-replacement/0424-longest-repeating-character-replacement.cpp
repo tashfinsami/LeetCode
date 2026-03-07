1786a421c73c8bef93a44d89007a1efbb8cb79b7
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-08 01:12:38 +0600

    Time: 13 ms (20.8%), Space: 10.9 MB (40.35%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        unordered_map<char, int> freq;
+        int max_freq = 0;
+        int res = 0;
+        int l = 0, r = 0;
+        while(r < s.size()) {
+            freq[s[r]]++;
+            max_freq = max(max_freq, freq[s[r]]);
+            if(r - l + 1 > max_freq + k) {
+                freq[s[l]]--;
+                l++;
+            }
+            res = max(res, r - l + 1);
+            r++;
+        }
+        return res;
+    }
+};
