10b6ae793c3982f0ff842fc5433f2b5572e4ef2c
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-09 16:57:53 +0600

    Time: 3 ms (66.29%), Space: 10 MB (69.64%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> makeFreq(string s, int l, int r) {
-        vector<int> freq(26, 0);
-        for(int i = l; i <= r; i++) freq[s[i] - 'a']++;
-        return freq;
-    }
     bool checkInclusion(string s1, string s2) {
         if(s1.size() > s2.size()) return false;
-        vector<int> f1 = makeFreq(s1, 0, s1.size() - 1);
+        vector<int> f1(26, 0);
+        vector<int> f2(26, 0);
+        for(int i = 0; i < s1.size(); i++) {
+            f1[s1[i] - 'a']++;
+            f2[s2[i] - 'a']++;
+        }
         int l = 0, r = s1.size() - 1;
-        vector<int> f2 = makeFreq(s2, l, r);
         while(r < s2.size()) {
             if(f2 == f1) return true;
             f2[s2[l] - 'a']--;
