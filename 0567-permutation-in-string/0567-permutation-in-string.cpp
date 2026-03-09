851e58684fb7681df9505e0ffd3b32d94576720d
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-09 15:37:12 +0600

    Time: 399 ms (8.71%), Space: 389 MB (5.04%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string makeKey(string s, int l, int r) {
+        vector<int> freq(26, 0);
+        for(int i = l; i <= r; i++) freq[s[i] - 'a']++;
+        string key = to_string(freq[0]);
+        for(int i = 1; i < 26; i++) key += ',' + to_string(freq[i]);
+        return key;
+    }
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size()) return false;
+        string k1 = makeKey(s1, 0, s1.size() - 1);
+        int l = 0, r = s1.size() - 1;
+        while(r < s2.size()) {
+            string k2 = makeKey(s2, l, r);
+            if(k2 == k1) return true;
+            l++;
+            r++;
+        }
+        return false;
+    }
+};
