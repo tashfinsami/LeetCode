e05cdd46bee4a4bb2300c5dd82d5bcba93baf700
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-25 02:07:04 +0600

    Time: 0 ms (100%), Space: 14.2 MB (49.28%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         while(l < r) {
             int m = l + (r - l) / 2;
             if(m - 1 >= 0 && nums[m] < nums[m - 1]) return nums[m];
-            if(m + 1 < nums.size() && nums[m] > nums[m + 1]) return nums[m + 1];
+            else if(m + 1 < nums.size() && nums[m] > nums[m + 1]) return nums[m + 1];
             if(nums[m] >= nums[l]) l = m + 1;
             else r = m - 1;
         }
