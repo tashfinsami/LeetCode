1b4ae12b06c3c499180237a20234c014a269e895
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-27 00:11:46 +0600

    Time: 48 ms (67.87%), Space: 136.7 MB (71.35%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -11,16 +11,16 @@ public:
     
     string get(string key, int timestamp) {
         vector<pair<string, int>>& data = rec[key];
-        if(!data.size() || timestamp < data[0].second) return "";
         int l = 0, r = data.size() - 1;
-        int index;
+        int index = -1;
         while(l <= r) {
             int m = l + (r - l) / 2;
-            index = m;
-            if(data[m].second >= timestamp) break;
-            else if(data[m].second < timestamp) l = m + 1;
+            if(data[m].second <= timestamp) {
+                index = m;
+                l = m + 1;
+            }
+            else r = m - 1;
         }
-        return (data[index].second <= timestamp) ? data[index].first : data[index - 1].first;
-
+        return (index == -1) ? "" : data[index].first;
     }
 };
