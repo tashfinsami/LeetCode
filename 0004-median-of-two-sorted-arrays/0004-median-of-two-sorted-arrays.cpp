10701bc5aa3a8b1344998ec4be52b539ba68bc91
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-28 18:31:40 +0600

    Time: 0 ms (100%), Space: 95 MB (94.3%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,72 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
-        vector<int> nums;
+        int size1 = nums1.size(), size2 = nums2.size();
+        int median = (size1 + size2) / 2 + 1;
+        int extra;
+        int res;
+        int count = 0;
+        bool done = false;
         int a = 0, b = 0;
-        while(a < m && b < n) {
-            //if(nums1[a] <= nums2[b]) {
-                while(a < m && b < n && nums1[a] <= nums2[b]) {
-                    nums.push_back(nums1[a]);
-                    a++;
+        while(a < size1 && b < size2) {
+            int start, end;
+            int check_arr = 0;
+            if(nums1[a] <= nums2[b]) {
+                int target = nums2[b];
+                int l = a; int r = size1 - 1;
+                while(l <= r) {
+                    int m = l + (r - l) / 2;
+                    if(nums1[m] > target) r = m - 1;
+                    else l = m + 1;  
                 }
-            //}
-            //else {
-                while(a < m && b < n && nums2[b] <= nums1[a]) {
-                    nums.push_back(nums2[b]);
-                    b++;
+                start = a; end = r;
+                a = r + 1;
+            }
+            else {
+                int target = nums1[a];
+                int l = b; int r = size2 - 1;
+                while(l <= r) {
+                    int m = l + (r - l) / 2;
+                    if(nums2[m] > target) r = m - 1;
+                    else l = m + 1;  
                 }
-            //}
+                start = b; end = r;
+                check_arr = 1;
+                b = r + 1;
+            }
+            int range = end - start + 1;
+            if(count + range < median) {
+                if(!check_arr) extra = nums1[end];
+                else extra = nums2[end];
+                count += range;
+            }
+            else {
+                int ptr = median - count - 1;
+                if(!check_arr) {
+                    res = nums1[start + ptr];
+                    if(ptr) extra = nums1[start + ptr - 1];
+                }
+                else {
+                    res = nums2[start + ptr];
+                    if(ptr) extra = nums2[start + ptr - 1];
+                }
+                done = true;
+                break;
+            }
         }
-        while(a < m) {
-            nums.push_back(nums1[a]);
-            a++;
+        if(!done && a < size1) {
+            int ptr = median - count - 1;
+            res = nums1[a + ptr];
+            if(ptr) extra = nums1[a + ptr - 1];
         }
-        while(b < n) {
-            nums.push_back(nums2[b]);
-            b++;
+        if(!done && b < size2) {
+            int ptr = median - count - 1;
+            res = nums2[b + ptr];
+            if(ptr) extra = nums2[b + ptr - 1];
         }
-        int len = m + n;
-        if(!(len % 2)) {
-            double avg = (double)(nums[len / 2 - 1] + nums[len / 2]) / 2;
-            return avg;
-        }
-        else return nums[len / 2];
+        if((size1 + size2) % 2) return (double)res;
+        else return ((double)res + extra) / 2;
     }
 };
 
-// 1 6     2 3 4 5
-
-
+// 1 3 7     6 8 10
