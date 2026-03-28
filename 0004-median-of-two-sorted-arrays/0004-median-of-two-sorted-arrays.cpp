6a23a485a8514bb9f9d67456c56d9cbfcfcc1e37
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-03-28 19:45:15 +0600

    Time: 5 ms (12.68%), Space: 95.2 MB (44.1%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -3,69 +3,57 @@ public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int size1 = nums1.size(), size2 = nums2.size();
         int median = (size1 + size2) / 2 + 1;
-        int extra;
-        int res;
+        vector<int> res(2);
         int count = 0;
         bool done = false;
         int a = 0, b = 0;
         while(a < size1 && b < size2) {
-            int start, end;
+            int l, r, target;
             bool check_arr1 = false;
             if(nums1[a] <= nums2[b]) {
-                int target = nums2[b];
-                int l = a; int r = size1 - 1;
-                while(l <= r) {
-                    int m = l + (r - l) / 2;
-                    if(nums1[m] > target) r = m - 1;
-                    else l = m + 1;  
-                }
-                start = a; end = r;
+                target = nums2[b];
+                l = a;
+                r = size1 - 1;
                 check_arr1 = true;
-                a = r + 1;
             }
             else {
-                int target = nums1[a];
-                int l = b; int r = size2 - 1;
-                while(l <= r) {
-                    int m = l + (r - l) / 2;
-                    if(nums2[m] > target) r = m - 1;
-                    else l = m + 1;  
-                }
-                start = b; end = r;
-                b = r + 1;
+                target = nums1[a];
+                l = b;
+                r = size2 - 1;
             }
+            vector<int>& temp = (check_arr1) ? nums1 : nums2;
+            while(l <= r) {
+                int m = l + (r - l) / 2;
+                if(temp[m] > target) r = m - 1;
+                else l = m + 1;  
+            }
+            int start = (check_arr1) ? a : b, end = r;
+            if(check_arr1) a = r + 1;
+            else b = r + 1;
             int range = end - start + 1;
             if(count + range < median) {
-                if(check_arr1) extra = nums1[end];
-                else extra = nums2[end];
+                res[0] = temp[end];
                 count += range;
             }
             else {
                 int ptr = median - count - 1;
-                if(check_arr1) {
-                    res = nums1[start + ptr];
-                    if(ptr) extra = nums1[start + ptr - 1];
-                }
-                else {
-                    res = nums2[start + ptr];
-                    if(ptr) extra = nums2[start + ptr - 1];
-                }
+                res[1] = temp[start + ptr];
+                if(ptr > 0) res[0] = temp[start + ptr - 1];
                 done = true;
                 break;
             }
         }
         if(!done && a < size1) {
             int ptr = median - count - 1;
-            res = nums1[a + ptr];
-            if(ptr) extra = nums1[a + ptr - 1];
+            res[1] = nums1[a + ptr];
+            if(ptr > 0) res[0] = nums1[a + ptr - 1];
         }
         if(!done && b < size2) {
             int ptr = median - count - 1;
-            res = nums2[b + ptr];
-            if(ptr) extra = nums2[b + ptr - 1];
+            res[1] = nums2[b + ptr];
+            if(ptr > 0) res[0] = nums2[b + ptr - 1];
         }
-        if((size1 + size2) % 2) return (double)res;
-        else return ((double)res + extra) / 2;
+        return ((size1 + size2) % 2) ? (double)res[1] : (double)(res[0] + res[1]) / 2;
     }
 };
 
