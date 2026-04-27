ca554530b14031349d92f60694279247cae1ee4b
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-04-27 20:17:27 +0600

    Time: 11 ms (12.75%), Space: 23.4 MB (48.11%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = 9;
-        unordered_map<int, unordered_set<char>> row_idx;
-        unordered_map<int, unordered_set<char>> col_idx;
-        map<pair<int, int>, unordered_set<int>> blk_idx;
-
+        set<pair<char, int>> row_key;
+        set<pair<char, int>> col_key;
+        set<pair<char, pair<int, int>>> blk_key;
         for(int row = 0; row < n; row++) {
             for(int col = 0; col < n; col++) {
                 char ch = board[row][col];
                 if(ch == '.') continue;
-                
-                pair<int, int> blk = {row / 3, col / 3};
+                if(row_key.find({ch, row}) != row_key.end()) return false;
+                if(col_key.find({ch, col}) != col_key.end()) return false;
+                if(blk_key.find({ch, {row / 3, col / 3}}) != blk_key.end()) return false;
 
-                if(row_idx[row].count(ch)) return false;
-                if(col_idx[col].count(ch)) return false;
-                if(blk_idx[blk].count(ch)) return false;
-
-                row_idx[row].insert(ch);
-                col_idx[col].insert(ch);
-                blk_idx[blk].insert(ch);
+                row_key.insert({ch, row});
+                col_key.insert({ch, col});
+                blk_key.insert({ch, {row / 3, col / 3}});
             }
         }
         return true;
