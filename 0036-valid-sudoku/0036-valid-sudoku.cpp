ba503f32ecca2350c232ee5d6fd34b7b10c5d748
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-04-29 00:10:55 +0600

    Time: 0 ms (100%), Space: 22.5 MB (61.96%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -2,34 +2,25 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         int n = 9;
-        struct pair_hash {
-            size_t operator()(const pair<char, int>& p) const {
-                return hash<char>()(p.first) ^ (hash<int>()(p.second) << 1);
+        int idx[3][9][9];
+        for(int mode = 0; mode < 3; mode++) {
+            for(int row = 0; row < n; row++) {
+                for(int col = 0; col < n; col++) {
+                    idx[mode][row][col] = 0;
+                }
             }
-        };
-        struct nested_pair_hash {
-            size_t operator()(const pair<char, pair<int,int>>& p) const {
-                size_t h1 = hash<char>()(p.first);
-                size_t h2 = hash<int>()(p.second.first);
-                size_t h3 = hash<int>()(p.second.second);
-                return h1 ^ (h2 << 1) ^ (h3 << 2);
-            }
-        };
-
-        unordered_set<pair<char, int>, pair_hash> row_key;
-        unordered_set<pair<char, int>, pair_hash> col_key;
-        unordered_set<pair<char, pair<int,int>>, nested_pair_hash> blk_key;
+        }
         for(int row = 0; row < n; row++) {
             for(int col = 0; col < n; col++) {
-                char ch = board[row][col];
-                if(ch == '.') continue;
-                if(row_key.find({ch, row}) != row_key.end()) return false;
-                if(col_key.find({ch, col}) != col_key.end()) return false;
-                if(blk_key.find({ch, {row / 3, col / 3}}) != blk_key.end()) return false;
+                if(board[row][col] == '.') continue;
+                int num = board[row][col] - '0' - 1;
+                if(idx[0][row][num]) return false;
+                if(idx[1][col][num]) return false;
+                if(idx[2][(row / 3) * 3 + (col / 3)][num]) return false;
 
-                row_key.insert({ch, row});
-                col_key.insert({ch, col});
-                blk_key.insert({ch, {row / 3, col / 3}});
+                idx[0][row][num] = 1;
+                idx[1][col][num] = 1;
+                idx[2][(row / 3) * 3 + (col / 3)][num] = 1;
             }
         }
         return true;
