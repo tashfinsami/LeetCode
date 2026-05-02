3aff203ba22277fcbf59606a4c93c82038e891f2
Tashfin Sami <135386400+tashfinsami@users.noreply.github.com>
2026-05-02 19:33:27 +0600

    Time: 0 ms (100%), Space: 12.5 MB (25.35%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -11,64 +11,18 @@
  */
 
 class Solution {
-private:
-    void bfs_left_fill(TreeNode* node, queue<int> &qval) {
-        node->val = qval.front();
-        qval.pop();
-        queue<TreeNode*> q;
-        q.push(node);
-        while(!q.empty()) {
-            TreeNode* cur = q.front();
-            q.pop();
-            if(!qval.empty()) {
-                int lval = qval.front();
-                qval.pop();
-                if(lval == INT_MAX) cur->left = nullptr;
-                else {
-                    if(!cur->left) cur->left = new TreeNode(lval);
-                    else cur->left->val = lval;
-                    q.push(cur->left);
-                }
-            }
-            if(!qval.empty()) {
-                int rval = qval.front();
-                qval.pop();
-                if(rval == INT_MAX) cur->right = nullptr;
-                else {
-                    if(!cur->right) cur->right = new TreeNode(rval);
-                    else cur->right->val = rval;
-                    q.push(cur->right);
-                }
-            }
-        }
-        return;
-    }
-    void bfs_right_extract(TreeNode* node, queue<int> &qval) {
-        qval.push(node->val);
-        queue<TreeNode*> q;
-        q.push(node);
-        while(!q.empty()) {
-            TreeNode* cur = q.front();
-            q.pop();
-            //if(!cur->left && !cur->right) continue; 
-            if(cur->right){
-                qval.push(cur->right->val);
-                q.push(cur->right);
-            }
-            else qval.push(INT_MAX);
-            if(cur->left) {
-                qval.push(cur->left->val);
-                q.push(cur->left);
-            }
-            else qval.push(INT_MAX);
-        }
-    }
 public:
-    TreeNode* invertTree(TreeNode* root) { // without node swapping
-        if(!root) return root;
-        queue<int> qval;
-        bfs_right_extract(root, qval);
-        bfs_left_fill(root, qval);
+    TreeNode* invertTree(TreeNode* root) { 
+        if(!root) return nullptr;
+        queue<TreeNode*> buffer;
+        buffer.push(root);
+        while(!buffer.empty()) {
+            TreeNode* cur = buffer.front();
+            buffer.pop();
+            swap(cur->left, cur->right);
+            if(cur->left) buffer.push(cur->left);
+            if(cur->right) buffer.push(cur->right);
+        }
         return root;
     }
 };
